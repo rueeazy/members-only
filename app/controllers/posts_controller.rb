928266bffec373439e190fdc7c6e1e3b0f0ff7fc
Christian Ruiz <santacruz.crm@gmail.com>
2020-07-01 18:19:08 -0700

    Database edits

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.all.reverse
   end
 
   # GET /posts/1
